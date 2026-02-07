5e5df21650cb25e294d931d95aae491eec07d849
dogofrog <obrazumov.ilya@yandex.ru>
2026-02-07 22:02:26 +0300

    publishedUnitySmashed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/internet_game_Build/',
   build: {
     rollupOptions: {
       input: {
